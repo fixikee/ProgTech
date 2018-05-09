0b825e228b75f0d63d66b50d88635af777397ee4
fixikee <renato.kazinczi@gmail.com>
2018-05-10 00:39:17 +0200

    basic menu

---
+++ ProgTech_game/app/src/main/java/beadando/game/MainActivity.java
@@ -0,0 +1,42 @@
+package beadando.game;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+
+import java.util.ConcurrentModificationException;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnOption;
+    String Difficulty;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnOption = findViewById(R.id.btnOption);
+        Difficulty = "easy";
+    }
+
+    public void btnSubmit_OnClick(View v) {
+        Intent intent = new Intent(MainActivity.this, beadando.game.Option.class);
+        startActivity(intent);
+    }
+
+    public void btnStart_OnClick(View v) {
+        Difficulty = getIntent().getStringExtra("dif");
+
+        Intent intent = new Intent(MainActivity.this, beadando.game.start.class);
+        intent.putExtra("difficulty", Difficulty);
+        startActivity(intent);
+    }
+
+}
+
